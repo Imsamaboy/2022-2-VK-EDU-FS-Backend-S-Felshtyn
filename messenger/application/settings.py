4216e902712293df3f713d124148c0356ff20247
Imsamaboy <sfelshtyn@gmail.com>
2022-10-25 18:26:40 +0300

    Delete unnecessary data

--- messenger/application/settings.py
+++ messenger/application/settings.py
@@ -79,8 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'messenger',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }

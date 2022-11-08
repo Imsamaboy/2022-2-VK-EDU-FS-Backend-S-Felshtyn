b88cd4b8488e7eeee9d20b1d88c67417d86cff18
Imsamaboy <sfelshtyn@gmail.com>
2022-11-08 21:16:41 +0300

    Added solution for 6 task

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -1,27 +1,36 @@
+import datetime
+
 from django.db import models
 
 from users.models import User
 
 
 class Chats(models.Model):
-    first_user = models.ForeignKey(User,
-                                   on_delete=models.SET_NULL,
-                                   null=True,
-                                   related_name='chat_first_user_name',
-                                   verbose_name="Собеседник 1")
-
-    second_user = models.ForeignKey(User, on_delete=models.SET_NULL,
-                                    null=True,
-                                    related_name='chat_second_user_name',
-                                    verbose_name="Собеседник 2")
+    users = models.ManyToManyField(User,
+                                   related_name='chat_user',
+                                   verbose_name="Собеседники")
 
     created_date = models.DateTimeField(auto_now_add=True,
-                                        verbose_name="Дата создания")
+                                        verbose_name="Дата создания",
+                                        null=False)
 
-    mute_notifications = models.BooleanField(null=False, verbose_name="Уведомления")
+    mute_notifications = models.BooleanField(verbose_name="Уведомления",
+                                             default=False,
+                                             null=False)
+
+    chat_name = models.TextField(verbose_name="Название чата",
+                                 null=False)
+
+    chat_description = models.TextField(verbose_name="Описание",
+                                        null=False)
 
     def __str__(self):
-        return f"id: {self.id}, user_1: {self.first_user}, user_2: {self.second_user}"
+        return f"""
+        id: {self.id}, 
+        users: {self.users}, 
+        chat_name: {self.chat_name}, 
+        chat_description: {self.chat_description}
+        """
 
     class Meta:
         verbose_name = 'Чат'
@@ -39,13 +48,14 @@ class Message(models.Model):
                                related_name='sender',
                                verbose_name="Отправитель")
 
-    recipient = models.ForeignKey(User,
-                                  on_delete=models.SET_NULL,
-                                  null=True,
-                                  related_name='recipient',
-                                  verbose_name="Получатель")
     message = models.TextField(verbose_name="Сообщение")
-    send_time = models.DateTimeField(auto_now_add=True, verbose_name="Время отправления")
+
+    send_time = models.DateTimeField(auto_now_add=True,
+                                     verbose_name="Время отправления")
+
+    is_read = models.BooleanField(default=False,
+                                  null=False,
+                                  verbose_name="Прочитано")
 
     class Meta:
         verbose_name = 'Сообщение'
--- messenger/users/models.py
+++ messenger/users/models.py
@@ -3,12 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    phone_number = models.TextField(verbose_name="Номер телефона", blank=True)
-    last_seen_at = models.DateTimeField(verbose_name="Дата последнего посещения ресурса", auto_now=True)
-    description = models.CharField(max_length=100, verbose_name="Описание профиля")
+    phone_number = models.TextField(verbose_name="Номер телефона",
+                                    blank=True)
+    last_seen_at = models.DateTimeField(verbose_name="Дата последнего посещения ресурса",
+                                        auto_now=True)
+    description = models.CharField(max_length=100,
+                                   verbose_name="Описание профиля")
 
     def __str__(self):
-        return f"username: {self.username}, email: {self.email}, phone: {self.phone_number}"
+        return f"""
+        username: {self.username}, 
+        email: {self.email},
+        phone: {self.phone_number}
+        """
 
     class Meta:
         verbose_name = 'Пользователь'

be942fea5d77a52c42c0b3f9072f98d98ce93e19
Imsamaboy <sfelshtyn@gmail.com>
2022-11-08 17:37:40 +0300

    Added 5 task solution

--- messenger/chats/urls.py
+++ messenger/chats/urls.py
@@ -1,9 +1,16 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.get_chats_list, name='get_chats_list'),
-    path("create/", views.create_chat, name='create_chat'),
-    path("chat/<int:id>/", views.get_chat_page, name='get_chat_page'),
+    path("create_chat/", views.create_chat, name="create_chat"),
+    path("get_chat/<int:chat_id>", views.get_chat, name="get_chat"),
+    path("delete_chat/<int:chat_id>", views.delete_chat, name="delete_chat"),
+    path("create_message/", views.create_message, name="create_message"),
+    path("get_message/<int:message_id>", views.get_message, name="get_message"),
+    path("get_user_chats/<int:user_id>", views.get_user_chats, name="get_user_chats"),
+    path("get_messages_by_chat_and_user_id/", views.get_messages_by_chat_and_user_id,
+         name="get_messages_by_chat_and_user_id"),
+    path("update_notification_status_in_chat/<int:chat_id>",
+         views.update_notification_status_in_chat,
+         name="update_notification_status_in_chat"),
 ]
